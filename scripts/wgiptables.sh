8de8ca47d84fbbac7576dd839b4cf25ceec8d40f
Pund-it <wayne.ledrew@tech-method.com>
2025-01-01 09:49:02 -0500

    Fixed a few configs and added WG portforwards
    Port forwards are now in place from the WG network to local stack network.

--- scripts/wgiptables.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
--- wg/custom-cont-init.d/iptables-setup.sh
+++ wg/custom-cont-init.d/iptables-setup.sh
@@ -3,3 +3,12 @@
 
 # Add NAT rule for traffic routing through WireGuard
 iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
+#Custom Port forwards
+iptables -t nat -A PREROUTING -i wg0 -p tcp --dport 3000 -j DNAT --to-destination 192.168.254.2
+iptables -t nat -A PREROUTING -i wg0 -p tcp --dport 9090 -j DNAT --to-destination 192.168.254.4
+iptables -t nat -A PREROUTING -i wg0 -p tcp --dport 8086 -j DNAT --to-destination 192.168.254.8
+iptables -t nat -A PREROUTING -i wg0 -p tcp --dport 3100 -j DNAT --to-destination 192.168.254.3
+iptables -t nat -A PREROUTING -i wg0 -p tcp --dport 1514 -j DNAT --to-destination 192.168.254.5
+iptables -t nat -A PREROUTING -i wg0 -p tcp --dport 9090 -j DNAT --to-destination 192.168.254.5
+iptables -t nat -A PREROUTING -i wg0 -p tcp --dport 9116 -j DNAT --to-destination 192.168.254.9
+iptables -t nat -A PREROUTING -i wg0 -p tcp --dport 8125 -j DNAT --to-destination 192.168.254.7
